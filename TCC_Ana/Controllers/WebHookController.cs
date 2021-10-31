273a62e4f03b7483f0e65450a54366923ae2bced
Leonaldo B Junior <leonaldo.barbosa@gmail.com>
2021-10-31 09:48:58 -0300

    create .net core web api project

---
+++ TCC_Ana/Controllers/WebHookController.cs
@@ -0,0 +1,65 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+
+
+namespace TCC_Ana.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebHookController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+        public  WebHookController(IConfiguration iConfig)
+        {
+            _configuration = iConfig;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var Options = new SecretClientOptions()
+            {
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+
+            var VaultClient = new SecretClient(new Uri(_configuration.GetValue<string>("VaultUri")), new DefaultAzureCredential(), Options);
+
+            //return Ok(_configuration.GetValue<string>("LeonaldoTest"));
+            return Ok(VaultClient.GetSecret("LeonaldoTest").Value.Value);
+
+        }
+
+        // POST: EndDeviceEvent/
+        [HttpPost]
+        //public async Task<IActionResult> Post([FromBody] collection)
+        public async Task<IActionResult> Post()
+        {   
+            //EndDeviceEventFirebase endDeviceEventFirebase = new EndDeviceEventFirebase();
+            //endDeviceEventFirebase.application_id = collection.end_device_ids.application_ids.application_id;
+            //endDeviceEventFirebase.dev_addr = collection.end_device_ids.dev_addr;
+            //endDeviceEventFirebase.dev_eui = collection.end_device_ids.dev_eui;
+            //endDeviceEventFirebase.device_id = collection.end_device_ids.device_id;
+            //endDeviceEventFirebase.eui = collection.uplink_message.rx_metadata[0].gateway_ids.eui;
+            //endDeviceEventFirebase.f_cnt = collection.uplink_message.f_cnt;
+            //endDeviceEventFirebase.frm_payload = collection.uplink_message.frm_payload;
+            //endDeviceEventFirebase.gateway_id = collection.uplink_message.rx_metadata[0].gateway_ids.gateway_id;
+            //endDeviceEventFirebase.received_at = collection.received_at;
+
+            //CollectionReference collectionReference = _fireStoreDb.Collection("end-device-webhook");
+            //await collectionReference.AddAsync(endDeviceEventFirebase);
+
+            return Ok();
+        }
+    }
+}
---
+++ TCC_Ana/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Azure.KeyVault;
+
+namespace TCC_Ana
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        })
+        //        .ConfigureAppConfiguration((context, config) =>
+        //        {
+        //            var settings = config.Build();
+        //            var keyVaultEndpoint = settings["VaultUri"];
+
+        //            var keyVaultClient = new KeyVaultClient(async (authority, resource, scope) =>
+        //            {
+        //                var credentials = new DefaultAzureCredential(false);
+        //                var token = credentials.GetToken(
+        //                    new TokenRequestContext(
+        //                        new[] { "https://vault.azure.net/.default" }));
+
+        //                return token.Token; 
+
+        //            });
+
+        //            config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+
+
+        //        });
+
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var settings = config.Build();
+                var keyVaultEndpoint = settings["VaultUri"];
+
+                config.AddAzureKeyVault(keyVaultEndpoint, new DefaultKeyVaultSecretManager());
+
+
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
+    
